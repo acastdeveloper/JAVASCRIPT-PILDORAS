b6ee7af5f03117a1d91ac7a2bd9a9b334cb11a4a
Àlex Castelló <ac.desenvolupador@gmail.com>
2020-01-31 12:08:58 +0100

    Video 3 Finalitzat. Col·locació de javascripts en fitxers externs i en CDN.

---
+++ 03/extern.js
@@ -0,0 +1,5 @@
+document.write("hola des d'arxiu extern");
+/*
+L'avantatge de posar javascript a un ariu extern és que el podem reutilitzar moltes vegades des de diverses pàgines de forma centralitzada, de tal manera que quan haig de fer un canvi no haig de canviar-lo en 100 pàgines sino només en aquest arxiu.
+
+*/
